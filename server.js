1950dd92095a30ac917f5e6f5fbffde054a33aa8
Umesh Karki <54566460+1umesh1@users.noreply.github.com>
2024-05-05 09:33:47 -0400

    new changes

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const {MongoClient} =require("mongodb");
 // Accessing the path module
 const path = require("path");
 
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+// // Step 1:
+// app.use(express.static(path.resolve(__dirname, "./client/build")));
+// // Step 2:
+// app.get("*", function (request, response) {
+//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+// });
 
 const articlesInfo ={
     "learn-react":{
