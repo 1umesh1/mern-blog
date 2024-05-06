726e5daa567378623040a0db40b3c333809c1651
Umesh Karki <54566460+1umesh1@users.noreply.github.com>
2024-05-06 12:06:46 -0400

    make it better

--- server.js
+++ server.js
@@ -54,7 +54,16 @@ MongoClient.connect(process.env.MONGO_URI).catch((err) => console.log(err))
 //  app.get('/hello/:name', (req,res)=> res.send(`Hello ${req.params.name}`));
 //  app.post('/', (req,res)=> res.send(`Hello, ${req.body.name}!`));
 
-
+if(process.env.NODE_ENV==="production"){
+    // Step 1:
+    app.use(express.static(path.resolve(__dirname, "./client/build")));
+    // Step 2:
+    app.get("*", function (request, response) {
+      response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    });
+    }
+    
+ app.listen(PORT,()=> console.log(`Server stared at port ${PORT}`));
 
  const withDB = async(operations,res )=> {
     try{ 
@@ -103,19 +112,5 @@ MongoClient.connect(process.env.MONGO_URI).catch((err) => console.log(err))
         const updateArticleInfo= await db.collection('articles')
         .findOne({name:articleName});
         res.status(200).json(updateArticleInfo);
-    }, res);
-
-
-
-    if(process.env.NODE_ENV==="production"){
-        // Step 1:
-        app.use(express.static(path.resolve(__dirname, "./client/build")));
-        // Step 2:
-        app.get("*", function (request, response) {
-          response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-        });
-        }
-    
-        
-     app.listen(process.env.PORT||8000,()=> console.log(`Server stared at port ${PORT}`));
+    }, res)
  })
